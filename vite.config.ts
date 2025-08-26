bb8bcae4b5c271ca5b33e69cc3aeb71ad653e58b
Dragos Ille <illedragos@yahoo.com>
2025-08-26 15:58:31 +0300

    fixed typo

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/reach-playground/",
+  base: "/react-playground/",
   server: {
     port: 3003,
   },
