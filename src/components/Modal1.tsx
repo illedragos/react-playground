65b961454065d673cffe02be158f0ea0c00317d9
Dragos Ille <illedragos@yahoo.com>
2025-09-04 19:13:30 +0300

    remove unnceesary imports

--- src/components/Modal1.tsx
+++ src/components/Modal1.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Modal, Box, Button, Fade, Backdrop } from "@mui/material";
 
 const style = {
--- src/examples/UseEffectExample.tsx
+++ src/examples/UseEffectExample.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Container, Typography, Box } from "@mui/material";
 
 export default function UseEffectExample() {
